32836c4f013b5d749c291e6a843125d54593b4dc
Sadaf Shaikh <sadaf.shaikh2819@gmail.com>
2024-12-12 11:22:02 +0500

    changed indentation of v6 launch file to make it consistent with other launch files. did this to do diff. launch file not tested yet.

--- src/basic_mobile_robot/launch/basic_mobile_bot_v6.launch.py
+++ src/basic_mobile_robot/launch/basic_mobile_bot_v6.launch.py
@@ -12,11 +12,11 @@ def generate_launch_description():
   # Set the path to different files and folders.
   pkg_share = FindPackageShare(package='basic_mobile_robot').find('basic_mobile_robot')
   default_model_path = os.path.join(pkg_share, 'models/basic_mobile_bot_v2.urdf')
-  robot_localization_file_path = os.path.join(pkg_share, 'config/ekf.yaml') 
+  robot_localization_file_path = os.path.join(pkg_share, 'config/ekf.yaml')
   robot_name_in_urdf = 'basic_mobile_bot'
   default_rviz_config_path = os.path.join(pkg_share, 'rviz/nav2_config.rviz')
-  nav2_dir = FindPackageShare(package='nav2_bringup').find('nav2_bringup') 
-  nav2_launch_dir = os.path.join(nav2_dir, 'launch') 
+  nav2_dir = FindPackageShare(package='nav2_bringup').find('nav2_bringup')
+  nav2_launch_dir = os.path.join(nav2_dir, 'launch')
   static_map_path = os.path.join(pkg_share, 'maps', 'map_name.yaml')
   nav2_params_path = os.path.join(pkg_share, 'params', 'nav2_params_hardware.yaml')
   nav2_bt_path = FindPackageShare(package='nav2_bt_navigator').find('nav2_bt_navigator')
@@ -38,7 +38,7 @@ def generate_launch_description():
   use_rviz = LaunchConfiguration('use_rviz')
   use_sim_time = LaunchConfiguration('use_sim_time')
 
-  # Declare the launch arguments  
+  # Declare the launch arguments
   declare_namespace_cmd = DeclareLaunchArgument(
     name='namespace',
     default_value='',
@@ -48,9 +48,9 @@ def generate_launch_description():
     name='use_namespace',
     default_value='False',
     description='Whether to apply a namespace to the navigation stack')
-      
+
   declare_autostart_cmd = DeclareLaunchArgument(
-    name='autostart', 
+    name='autostart',
     default_value='true',
     description='Automatically startup the nav2 stack')
 
@@ -58,22 +58,22 @@ def generate_launch_description():
     name='default_bt_xml_filename',
     default_value=behavior_tree_xml_path,
     description='Full path to the behavior tree xml file to use')
-      
+
   declare_map_yaml_cmd = DeclareLaunchArgument(
     name='map',
     default_value=static_map_path,
     description='Full path to map file to load')
-      
+
   declare_model_path_cmd = DeclareLaunchArgument(
-    name='model', 
-    default_value=default_model_path, 
+    name='model',
+    default_value=default_model_path,
     description='Absolute path to robot urdf file')
-  
+
   declare_params_file_cmd = DeclareLaunchArgument(
     name='params_file',
     default_value=nav2_params_path,
     description='Full path to the ROS2 parameters file to use for all launched nodes')
-  
+
   declare_rviz_config_file_cmd = DeclareLaunchArgument(
     name='rviz_config_file',
     default_value=default_rviz_config_path,
@@ -83,7 +83,7 @@ def generate_launch_description():
     name='slam',
     default_value='False',
     description='Whether to run SLAM')
-  
+
   declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
     name='use_robot_state_pub',
     default_value='True',
@@ -108,8 +108,8 @@ def generate_launch_description():
     executable='ekf_node',
     name='ekf_filter_node',
     output='screen',
-    parameters=[robot_localization_file_path, 
-                {'use_sim_time': use_sim_time}])
+    parameters=[robot_localization_file_path,
+    {'use_sim_time': use_sim_time}])
 
   # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
   start_robot_state_publisher_cmd = Node(
@@ -117,8 +117,8 @@ def generate_launch_description():
     package='robot_state_publisher',
     executable='robot_state_publisher',
     namespace=namespace,
-    parameters=[{'use_sim_time': use_sim_time, 
-                 'robot_description': Command(['xacro ', model])}])
+    parameters=[{'use_sim_time': use_sim_time,
+    'robot_description': Command(['xacro ', model])}])
 
   # Launch RViz
   start_rviz_cmd = Node(
@@ -127,7 +127,7 @@ def generate_launch_description():
     executable='rviz2',
     name='rviz2',
     output='screen',
-    arguments=['-d', rviz_config_file])    
+    arguments=['-d', rviz_config_file])
 
   #  Include the IMU launch file
   start_imu_cmd = IncludeLaunchDescription(
@@ -147,7 +147,7 @@ def generate_launch_description():
     launch_arguments={'use_sim_time': use_sim_time}.items()
   )
 
-  # #Include odometry 
+  # #Include odometry
   # start_odometry_cmd = IncludeLaunchDescription(
   #     PythonLaunchDescriptionSource(odometry_launch_path),
   #     launch_arguments={'use_sim_time': use_sim_time}.items()
@@ -157,14 +157,14 @@ def generate_launch_description():
   # Launch the ROS 2 Navigation Stack
   start_ros2_navigation_cmd = IncludeLaunchDescription(
     PythonLaunchDescriptionSource(os.path.join(nav2_launch_dir, 'bringup_launch.py')),
-    launch_arguments={'namespace': namespace,
-                      'use_namespace': use_namespace,
-                      'slam': slam,
-                      'map': map_yaml_file,
-                      'use_sim_time': use_sim_time,
-                      'params_file': params_file,
-                      'default_bt_xml_filename': default_bt_xml_filename,
-                      'autostart': autostart}.items())
+    launch_arguments = {'namespace': namespace,
+                        'use_namespace': use_namespace,
+                        'slam': slam,
+                        'map': map_yaml_file,
+                        'use_sim_time': use_sim_time,
+                        'params_file': params_file,
+                        'default_bt_xml_filename': default_bt_xml_filename,
+                        'autostart': autostart}.items())
 
   # Create the launch description and populate
   ld = LaunchDescription()
@@ -180,8 +180,8 @@ def generate_launch_description():
   ld.add_action(declare_params_file_cmd)
   ld.add_action(declare_rviz_config_file_cmd)
   ld.add_action(declare_slam_cmd)
-  ld.add_action(declare_use_robot_state_pub_cmd)  
-  ld.add_action(declare_use_rviz_cmd) 
+  ld.add_action(declare_use_robot_state_pub_cmd)
+  ld.add_action(declare_use_rviz_cmd)
 
   # Add any actions
   ld.add_action(start_robot_localization_cmd)
