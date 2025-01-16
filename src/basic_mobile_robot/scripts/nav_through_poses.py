448605bee2aca454ed1001a599a0b53beb502d1f
Sadaf Shaikh <sadaf.shaikh2819@gmail.com>
2025-01-16 15:35:06 +0500

    revised initial pose

--- src/basic_mobile_robot/scripts/nav_through_poses.py
+++ src/basic_mobile_robot/scripts/nav_through_poses.py
@@ -40,8 +40,8 @@ def main():
   initial_pose = PoseStamped()
   initial_pose.header.frame_id = 'map'
   initial_pose.header.stamp = navigator.get_clock().now().to_msg()
-  initial_pose.pose.position.x = 247.5839
-  initial_pose.pose.position.y = 103.9585
+  initial_pose.pose.position.x = 247.48319928540806
+  initial_pose.pose.position.y = 103.95711444744386
   initial_pose.pose.position.z = 0.0
   initial_pose.pose.orientation.x = 0.0
   initial_pose.pose.orientation.y = 0.0
