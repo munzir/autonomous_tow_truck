1164b6eef54c52594fb4eddf52f002bcc766b4cf
AliNisar00 <syed.alinisar2002@gmail.com>
2025-03-13 20:33:52 +0500

    Added dummy data generation to wheel_sensing_robust.h for wheel encoder data

--- src/arduino/wheels_sensing_testing/include/wheels_sensing_robust.h
+++ src/arduino/wheels_sensing_testing/include/wheels_sensing_robust.h
@@ -21,14 +21,10 @@ int sensingTickPeriod = 1; // by default we assume ticks to occur every 1 ms
 const float pi = 3.14159265358979323846;
 float last_frq = 0.0;
 // these are checked for in the main program
-volatile unsigned long
-    timerCounts; // apparently the volatile keyword is to let the compiler know
-                 // this can have very sharp changes? other than that, this is
-                 // self explanatory
+volatile unsigned long timerCounts;
 long tempCounter = 0;
-const uint16_t TIMER_THRESHOLD =
-    500; // ~0.5ms seconds at 16MHz with prescaler 1024
-unsigned int SENSING_LOOP_PERIOD = 50; // ms
+const uint16_t TIMER_THRESHOLD = 500;
+unsigned int SENSING_LOOP_PERIOD = 50;
 float frq;
 
 unsigned int timerTicks;
@@ -36,71 +32,54 @@ unsigned int timerPeriod;
 bool frq_updated = false;
 
 void startCounting(unsigned int ms) {
-  timerPeriod = ms; // how many 1 ms counts to do
-  timerTicks = 0;   // reset interrupt counter
+  timerPeriod = ms;
+  timerTicks = 0;
   tempCounter = 0;
-} // end of startCounting
+}
 
 void Timer1Reset() {
-  TCNT1 = 0;             // Reset the timer counter
-  TCCR1B |= (1 << CS11); // Restart the timer
+  TCNT1 = 0;
+  TCCR1B |= (1 << CS11);
 }
 
 void ai1() { Timer1Reset(); }
 
-//
-// Timer1 Compare Match Interrupt Service Routine
 ISR(TIMER1_COMPA_vect) {
-  TCCR1B &= ~(1 << CS11); // Stop the timer by clearing the prescaler bits
+  TCCR1B &= ~(1 << CS11);
   tempCounter++;
 }
 
 void WheelsSensingSetup(int tickPeriod) {
   sensingTickPeriod = tickPeriod;
-  startCounting(SENSING_LOOP_PERIOD); // Start counting for 10 ms (or another period)
-  pinMode(3, INPUT);  // internalเป็น pullup input pin 3
+  startCounting(SENSING_LOOP_PERIOD);
+  pinMode(3, INPUT);
   attachInterrupt(1, ai1, RISING);
-  // Configure Timer1
-  cli();                   // Disable interrupts during configuration
-  TCCR1A = 0;              // Normal mode, no PWM
-  TCCR1B = 0;              // Reset Timer1 configuration
-  TCNT1 = 0;               // Reset the counter
-  OCR1A = TIMER_THRESHOLD; // Set the compare match value (threshold)
-  TCCR1B |= (1 << WGM12);  // Enable CTC mode (Clear Timer on Compare Match)
-  TCCR1B |= (1 << CS11);   // Set prescaler to 1024
-
-  // Enable Timer1 compare match interrupt
+  cli();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+  OCR1A = TIMER_THRESHOLD;
+  TCCR1B |= (1 << WGM12);
+  TCCR1B |= (1 << CS11);
   TIMSK1 |= (1 << OCIE1A);
-  sei(); // Enable global
-} // end of setup
+  sei();
+}
 
 float CalculateAndThresholdFrequency() {
-  // adjust counts by counting interval to give frequency in Hz
-  frq = (timerCounts * 1000.0) /
-        timerPeriod; // frequency is obtained by dividing the number of detected
-                     // pulses by the period, then multiplying by 1000 because
-                     // the period is in ms
-  if (abs(last_frq - frq) > 900) {
-    frq = last_frq;
-  } else {
-    last_frq = frq;
-  }
+  // Overriding frequency with dummy data
+  frq = 0.0;
   frq_updated = true;
   return frq;
 }
 
 void SensingLoop() {
-  // see if we have reached timing period
   timerTicks += sensingTickPeriod;
   if (timerTicks < timerPeriod)
-    return; // not yet
+    return;
 
-  // calculate total count
   timerCounts = tempCounter;
-  // ======
   frq = CalculateAndThresholdFrequency();
-  // restart counting
-  startCounting(SENSING_LOOP_PERIOD); // Continue counting for 10 ms (or another period)
+  startCounting(SENSING_LOOP_PERIOD);
 }
 
 #endif // WHEELS_SENSING_ROBUST_H
