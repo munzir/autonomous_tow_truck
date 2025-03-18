82bbb08d98694227803c5e72244701b5c3040f85
Sadaf Shaikh <sadaf.shaikh2819@gmail.com>
2025-03-18 09:39:00 +0500

    stepwise checking

---
+++ src/basic_mobile_robot/launch/odometry.launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='py_pubsub',
+            executable='joyard',
+            output='screen',
+        ),
+        Node(
+            package='py_pubsub',
+            executable='freqnangle',
+            output='screen'
+        ),
+        Node(
+            package='py_pubsub',
+            executable='poser',
+            output='screen'
+        ),
+        Node(
+            package='py_pubsub',
+            executable='listener',
+            output='screen'
+        )
+    ])
