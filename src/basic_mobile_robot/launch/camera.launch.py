b58408750e0e26b60c59f5b573ff0d12feb103e7
Sadaf Shaikh <sadaf.shaikh2819@gmail.com>
2025-05-12 13:57:26 +0500

    javerias changes to paramter yaml

--- src/basic_mobile_robot/launch/camera.launch.py
+++ src/basic_mobile_robot/launch/camera.launch.py
@@ -13,6 +13,17 @@ def generate_launch_description():
             'enable_color': True,
             'align_depth.enable': True,
             'pointcloud.enable': True,
+            'pointcloud.stream_filter': 2,  # For better point density
+            'depth_module.profile': '640x480x30',  # Optimal resolution
+            # 'frameset': 'camera_depth_frame',  # Critical for TF consistency
+            # Frame IDs must match your TF tree
+            'base_frame_id': 'camera_link',
+            'depth_frame_id': 'camera_depth__optical_frame',
+            'infra_frame_id': 'camera_infra_frame',
+            'color_frame_id': 'camera_color_frame',
+            # Optional performance tuning
+            'depth_module.enable_auto_exposure': True,
+            'pointcloud.ordered_pc': False
         }]
     )
 
@@ -21,5 +32,10 @@ def generate_launch_description():
         executable='static_transform_publisher',
         arguments=['0.345', '0', '0.28', '0', '0', '0', 'base_link', 'camera_link']
     )
+    optical_tf = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=['0', '0', '0', '-1.5708', '0', '-1.5708', 'camera_link', 'camera_depth_optical_frame']
+    )
 
-    return LaunchDescription([realsense_node, static_tf])
+    return LaunchDescription([realsense_node, static_tf, optical_tf])
