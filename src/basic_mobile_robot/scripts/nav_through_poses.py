46abda12862e8c80e9002d262ce07ea243b218ea
Sadaf Shaikh <sadaf.shaikh2819@gmail.com>
2025-05-16 22:51:11 +0500

    final showcase working

--- src/basic_mobile_robot/scripts/nav_through_poses.py
+++ src/basic_mobile_robot/scripts/nav_through_poses.py
@@ -40,13 +40,13 @@ def main():
   initial_pose = PoseStamped()
   initial_pose.header.frame_id = 'map'
   initial_pose.header.stamp = navigator.get_clock().now().to_msg()
-  initial_pose.pose.position.x = 287.4729309082031
-  initial_pose.pose.position.y = -14.669928550720215
+  initial_pose.pose.position.x = 26.181461334228516
+  initial_pose.pose.position.y = -0.3705190122127533
   initial_pose.pose.position.z = 0.0
   initial_pose.pose.orientation.x = 0.0
   initial_pose.pose.orientation.y = 0.0
-  initial_pose.pose.orientation.z =-0.9999373025858765
-  initial_pose.pose.orientation.w = 0.01119780769976286
+  initial_pose.pose.orientation.z =0.9999538866331811
+  initial_pose.pose.orientation.w = 0.009603364368563292
   navigator.setInitialPose(initial_pose)
  
   # Activate navigation, if not autostarted. This should be called after setInitialPose()
@@ -94,7 +94,7 @@ def main():
   # path = navigator.getPathThroughPoses(initial_pose, goal_poses)
  
   # Go through the goal poses
-  navigator.goThroughPoses(goal_poses[0:26])
+  navigator.goThroughPoses(goal_poses[0:8])
  
   i = 0
   # Keep doing stuff as long as the robot is moving towards the goal poses
