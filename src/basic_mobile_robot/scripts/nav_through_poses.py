9ad237419fe7ff795bba0065fa1c548ffaefda15
Sadaf Shaikh <sadaf.shaikh2819@gmail.com>
2025-05-17 09:55:12 +0500

    final control showcase changes

--- src/basic_mobile_robot/scripts/nav_through_poses.py
+++ src/basic_mobile_robot/scripts/nav_through_poses.py
@@ -94,7 +94,7 @@ def main():
   # path = navigator.getPathThroughPoses(initial_pose, goal_poses)
  
   # Go through the goal poses
-  navigator.goThroughPoses(goal_poses[0:8])
+  navigator.goThroughPoses(goal_poses[0:7])
  
   i = 0
   # Keep doing stuff as long as the robot is moving towards the goal poses
