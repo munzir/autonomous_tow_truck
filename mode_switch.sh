f626db8e8e0fbf55beb19b51e83372061685f73f
Sadaf Shaikh <sadaf.shaikh2819@gmail.com>
2025-06-02 14:08:15 +0500

    added the mode switch as well

---
+++ mode_switch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source /opt/ros/humble/setup.bash
+source ~/autonomous_tow_truck/install/setup.bash
+source ~/autonomous_tow_truck/install/local_setup.bash
+
+# Launch the ROS 2 script
+ros2 launch basic_mobile_robot mode_switch.py
