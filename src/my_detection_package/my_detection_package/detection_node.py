a24bd56065094de9d5c80bc4fb9bb71246364af9
Abdullah Shaikh <abdullahshheikh2@gmail.com>
2025-07-30 08:27:42 +0500

    nothing

--- src/my_detection_package/my_detection_package/detection_node.py
+++ src/my_detection_package/my_detection_package/detection_node.py
@@ -67,7 +67,6 @@ class YoloDetectionAndPointCloudNode(Node):
                 X, Y, _ = rs.rs2_deproject_pixel_to_point(self.depth_intrinsics, [cx, cy], Z)
                 points.append([X, Y, Z])
 
-            # Prepare Detection2D message (not published, just for internal use)
             detection = Detection2D()
             detection.header = header
             detection.bbox.center.position.x = float(cx)
