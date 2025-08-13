2f48591e2ec8a76734dbb61359fc9e0be910344c
Sadaf Shaikh <sadaf.shaikh2819@gmail.com>
2025-08-13 14:59:16 +0500

    April tags working perfectly fine

--- src/basic_mobile_robot/launch/basic_mobile_bot_v5.launch.py
+++ src/basic_mobile_robot/launch/basic_mobile_bot_v5.launch.py
@@ -201,7 +201,6 @@ def generate_launch_description():
                 '/home/munzir/official-autonomous-tow-truck/autonomous_tow_truck/src/basic_mobile_robot/params/tags.yaml',
                 {
                 'image_transport': 'raw',
-                'image_topic': 'image_raw',
                 'camera_frame': 'camera_color_optical_frame',
                 'publish_tag_tf': True,
                 'tag_family': 'tag36h11',
@@ -209,7 +208,7 @@ def generate_launch_description():
                 'use_sim_time': False
             }],
             remappings=[
-                ('image_raw', '/camera/color/image_raw'),
+                ('image_rect', '/camera/camera/color/image_raw'),
                 ('camera_info', '/camera/color/camera_info')
             ]
   )
